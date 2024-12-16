57ae08bd6768010e43c0acf63a6365f327e2218d
vamsychalla <vamsichalla575@gmail.com>
2024-12-16 08:45:47 +0530

    test

--- 1-pr.sh
+++ 1-pr.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "Hello Boss How are you"
-echo "Enter user name"
+# echo "Hello Boss How are you"
+# echo "Enter user name"
+
+PERSON1=RAVI
+PERSON2=RAJ
+
+PERSON3=kk
+PERSON4=KL
+
+echo "$PERSON4:: HOW ARE YOU $PERSON2, $PERSON3"
+echo "$PERSON1:: You are forgot me $PERSON4"
