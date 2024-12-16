71d1bf39b134e19a13973e5d133540553043fc0a
vamsychalla <vamsichalla575@gmail.com>
2024-12-16 09:00:39 +0530

    test

--- 1-pr.sh
+++ 1-pr.sh
@@ -12,9 +12,17 @@
 # echo "$PERSON4:: HOW ARE YOU $PERSON2, $PERSON3"
 # echo "$PERSON1:: You are forgot me $PERSON4"
 
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 
-echo "Hi $PERSON1:: How are you $PERSON2"
-echo "$PERSON2:: HELLO $PERSON1 jaffa "
+# echo "Hi $PERSON1:: How are you $PERSON2"
+# echo "$PERSON2:: HELLO $PERSON1 jaffa "
+# echo "enter user name"
+
 echo "enter user name"
+read NAME
+
+echo "enter password"
+read -s PASSWD
+
+echo "USer name :: $NAME, Password::$PASSWD"
