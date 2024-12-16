a71197cd89059571a48816485ffa4ed91df8c8cf
vamsychalla <vamsichalla575@gmail.com>
2024-12-16 10:57:12 +0530

    test

--- 01-pr.sh
+++ 01-pr.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# PERSON1=RAVI
-# PERSON2=RAJu
-# echo "Hello this is sample world"
-# echo "$PERSON1:: HI $PERSON2, HOW ARE YOU"
-# echo "$PERSON2:: HELLOW $PERSON1, I am ok thanks what about you"
+# # PERSON1=RAVI
+# # PERSON2=RAJu
+# # echo "Hello this is sample world"
+# # echo "$PERSON1:: HI $PERSON2, HOW ARE YOU"
+# # echo "$PERSON2:: HELLOW $PERSON1, I am ok thanks what about you"
+
+# echo "Enter user name"
+# read UNAME
+
+# echo "Enter password"
+
+# read -s PASSWD
+
+# echo "user is : $UNAME and PASSWORD is : $PASSWD"
+
 
 echo "Enter user name"
 read UNAME
+echo "Enter 2nd mane"
+read UNAME2
 
-echo "Enter password"
+echo "$UNAME:: Hi $UNAME2, how are you"
+echo "$UNAME2:: Hello $UNAME, I am ok thank you, what about you"
 
-read -s PASSWD
-
-echo "user is : $UNAME and PASSWORD is : $PASSWD"
