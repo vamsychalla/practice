0a85357001ac22154b5626048391ef55a25d0586
vamsychalla <vamsichalla575@gmail.com>
2024-12-16 15:39:32 +0530

    test

--- DB.sh
+++ DB.sh
@@ -14,27 +14,27 @@ Validate(){
         }
 
 
-if [ $USER -ne 0 ]
-then 
-    echo "This script need to be run with root user or Admin credemtials"
-    exit 1
-else
-    echo "Going to install given requirement"
-fi
+# if [ $USER -ne 0 ]
+# then 
+#     echo "This script need to be run with root user or Admin credemtials"
+#     exit 1
+# else
+#     echo "Going to install given requirement"
+# fi
 
-dnf install mysql-server -y
-Validate $? "INSTALLATION of MYSQL server"
+# dnf install mysql-server -y
+# Validate $? "INSTALLATION of MYSQL server"
 
-systemctl enable mysqld
-Validate $? "Enable MYSQL server"
-systemctl start mysqld
-Validate $? "Starting the server"
+# systemctl enable mysqld
+# Validate $? "Enable MYSQL server"
+# systemctl start mysqld
+# Validate $? "Starting the server"
 
-mysql -h db.hnsc.fun -uroot -pExpenseApp@1 
-if [ $? -ne 0]
-then 
-    mysql_secure_installation --set-root-pass ExpenseApp@1
-    Validate S? "Mysql root password setup"
-else
-echo "already password was setup"
+# mysql -h db.hnsc.fun -uroot -pExpenseApp@1 
+# if [ $? -ne 0]
+# then 
+#     mysql_secure_installation --set-root-pass ExpenseApp@1
+#     Validate S? "Mysql root password setup"
+# else
+# echo "already password was setup"
 
