4ad3f6203f58c49440257252520b32ede03afd72
vamsychalla <vamsichalla575@gmail.com>
2024-12-16 10:48:07 +0530

    test

--- 01-pr.sh
+++ 01-pr.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-PERSON1=RAVI
-PERSON2=RAJu
-echo "Hello this is sample world"
-echo "$PERSON1:: HI $PERSON2, HOW ARE YOU"
-echo "$PERSON2:: HELLOW $PERSON1, I am ok thanks what about you"
+# PERSON1=RAVI
+# PERSON2=RAJu
+# echo "Hello this is sample world"
+# echo "$PERSON1:: HI $PERSON2, HOW ARE YOU"
+# echo "$PERSON2:: HELLOW $PERSON1, I am ok thanks what about you"
+
+PERSON1=$
+PERSON2=$
+
+echo "$PERSON1"
+echo "$PERSON2"
