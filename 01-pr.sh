88f751306b123ee76143138bb4bc82c220a99b15
vamsychalla <vamsichalla575@gmail.com>
2024-12-16 10:48:52 +0530

    test

--- 01-pr.sh
+++ 01-pr.sh
@@ -6,8 +6,8 @@
 # echo "$PERSON1:: HI $PERSON2, HOW ARE YOU"
 # echo "$PERSON2:: HELLOW $PERSON1, I am ok thanks what about you"
 
-PERSON1=$
-PERSON2=$
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1"
 echo "$PERSON2"
